9a996cfa291d17efe5ac6734a65c6db0eb091332
Geine <cho881020@gmail.com>
2021-05-15 14:41:08 +0900

    웹상의 이미지 불러오기 (Glide 활용)  - 인터넷 권한 획득 체험

--- app/src/main/java/com/neppplus/librarypractice_genie/MainActivity.kt
+++ app/src/main/java/com/neppplus/librarypractice_genie/MainActivity.kt
@@ -3,6 +3,7 @@ package com.neppplus.librarypractice_genie
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bumptech.glide.Glide
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -18,6 +19,12 @@ class MainActivity : AppCompatActivity() {
             startActivity(myIntent)
 
         }
+
+
+//        인터넷에 있는 이미지 (https:///...) 를 이미지뷰에 바로 대입.
+
+        Glide.with(this).load("https://neppplus.com/images/academy/pc_class_thumb_android.png").into(lectureImg1)
+
 
     }
 }
